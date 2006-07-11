713a01e934a3f7a8f7cc59dcd41cdbede2a719e3
lmoffitt <lmoffitt@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-07-11 01:05:03 +0000

    updated

---
+++ clients/cronos/applications/online_review/trunk/docs/assembly/scorecard/Assembly Packet/Architecture Deliverables/Data Model/Sequences.sql
@@ -0,0 +1,42 @@
+CREATE TABLE id_sequences (
+  name                  VARCHAR(255)    NOT NULL,
+  next_block_start      INTEGER         NOT NULL,
+  block_size            INTEGER         NOT NULL,
+  exhausted             INTEGER         NOT NULL,
+  PRIMARY KEY (name)
+);
+
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('scorecard_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('scorecard_group_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('scorecard_section_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('scorecard_question_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('project_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('phase_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('resource_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('upload_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('submission_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('review_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('review_item_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('review_comment_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('review_item_comment_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('project_audit_id_seq', 1, 20, 0);
+
+
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('screening_task_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('screening_result_id_seq', 1, 20, 0);
