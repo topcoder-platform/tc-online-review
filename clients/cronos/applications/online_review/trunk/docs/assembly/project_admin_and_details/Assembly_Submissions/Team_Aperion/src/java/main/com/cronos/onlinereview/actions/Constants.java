93fa1cc2c313e2fa86b7d0d8017e9f37e701e591
George1 <George1@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-09-05 14:29:05 +0000

    Added constan that defines the name of "edit" action-forward.

--- clients/cronos/applications/online_review/trunk/docs/assembly/project_admin_and_details/Assembly_Submissions/Team_Aperion/src/java/main/com/cronos/onlinereview/actions/Constants.java
+++ clients/cronos/applications/online_review/trunk/docs/assembly/project_admin_and_details/Assembly_Submissions/Team_Aperion/src/java/main/com/cronos/onlinereview/actions/Constants.java
@@ -512,6 +512,13 @@ public class Constants {
      * action forward.
      */
     public static final String PREVIEW_FORWARD_NAME = "preview";
+    
+    /**
+     * This member variable is a string constant that defines the name of the Edit commonly used
+     * action forward. Such an action foward is often used to redirect to Edit Screening or Edit
+     * Review, etc. page from the corresponding &quot;Create&quot; type of page.
+     */
+    public static final String EDIT_FORWARD_NAME = "edit";
 
 
     // Hidden constructor
