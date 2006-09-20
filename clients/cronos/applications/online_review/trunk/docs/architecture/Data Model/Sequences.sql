31d5997134e599ee7786c0530f7577e5e4a35841
George1 <George1@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-09-20 11:01:58 +0000

    Modified SQL scripts files to be in sync.

--- clients/cronos/applications/online_review/trunk/docs/architecture/Data Model/Sequences.sql
+++ clients/cronos/applications/online_review/trunk/docs/architecture/Data Model/Sequences.sql
@@ -21,9 +21,19 @@ INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
   VALUES('resource_id_seq', 1, 20, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('resource_role_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('notification_type_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
   VALUES('upload_id_seq', 1, 20, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('upload_type_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('upload_status_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
   VALUES('submission_id_seq', 1, 20, 0);
+INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
+  VALUES('submission_status_id_seq', 1, 20, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
   VALUES('review_id_seq', 1, 20, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
@@ -34,10 +44,6 @@ INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
   VALUES('review_item_comment_id_seq', 1, 20, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
   VALUES('project_audit_id_seq', 1, 20, 0);
-
-INSERT INTO id_sequences(name, next_block_start, block_size, exhausted) 
-  VALUES ('ip_server_id_seq', 100001, 20, 0);
-
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
   VALUES('screening_task_id_seq', 1, 20, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
