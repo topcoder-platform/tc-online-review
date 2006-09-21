aef6bf0cb0b772630ba61bd84b56fe512b311483
George1 <George1@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-09-21 23:20:15 +0000

    Reworked SQL scripts a little.

--- src/sql/X-6 - Clear Projects.sql
+++ src/sql/X-6 - Clear Projects.sql
@@ -1,7 +1,11 @@
 -- Note: this script will remove all information not only about all projects,
---       but about all submissions, uploads, resources, notifications and
---       phases as well.  You'll need to repopulate the databse with that
---       information using appropritate scripts.
+--       but about all phases, submissions, uploads, resources, notifications,
+--       automatic screening results, review, their items and comments as well.
+--       You'll need to repopulate the databse with that information using
+--       appropritate scripts.
+
+DELETE screening_result;
+DELETE screening_task;
 
 DELETE review_item_comment;
 DELETE review_item;
