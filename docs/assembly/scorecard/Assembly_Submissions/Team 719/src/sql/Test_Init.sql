09371c0ac265a290c32826a9dc7a05f56573f18b
dulery <dulery@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-09-28 18:16:18 +0000

    Removing scorecard admin from old location in svn

--- docs/assembly/scorecard/Assembly_Submissions/Team 719/src/sql/Test_Init.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO resource (resource_id, resource_role_id, create_user, create_date, modify_user, modify_date)
-			  VALUES (1000, 13, 'SYSTEM', CURRENT, 'SYSTEM', CURRENT);
-INSERT INTO resource_info (resource_id, resource_info_type_id, value, create_user, create_date, modify_user, modify_date)
-			  VALUES (1000, 1, 10, 'SYSTEM', CURRENT, 'SYSTEM', CURRENT);
