13ae67102255315b94a51e4d1ac8239345e05936
WishingBone <WishingBone@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-10-13 14:17:17 +0000

    Updated to create synonym for id_sequences

--- clients/cronos/applications/online_review/trunk/docs/architecture/Data Model/Synonyms.sql
+++ clients/cronos/applications/online_review/trunk/docs/architecture/Data Model/Synonyms.sql
@@ -1,5 +1,6 @@
 CREATE SYNONYM user FOR common_oltp:user;
 CREATE SYNONYM email FOR common_oltp:email;
+CREATE SYNONYM id_sequences FOR common_oltp:id_sequences;
 CREATE SYNONYM user_rating FOR tcs_catalog:user_rating;
 CREATE SYNONYM user_reliability FOR tcs_catalog:user_reliability;
 CREATE SYNONYM comp_catalog FOR tcs_catalog:comp_catalog;
