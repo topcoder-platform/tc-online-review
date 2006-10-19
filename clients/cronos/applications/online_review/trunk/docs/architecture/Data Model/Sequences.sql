c5d0e028d033c14a8a34b53ed47f9cbceefdbe4e
WishingBone <WishingBone@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-10-19 03:34:16 +0000

    Updated start/block size

--- clients/cronos/applications/online_review/trunk/docs/architecture/Data Model/Sequences.sql
+++ clients/cronos/applications/online_review/trunk/docs/architecture/Data Model/Sequences.sql
@@ -7,44 +7,44 @@
 -- );
 
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('scorecard_id_seq', 1, 20, 0);
+  VALUES('scorecard_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('scorecard_group_id_seq', 1, 20, 0);
+  VALUES('scorecard_group_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('scorecard_section_id_seq', 1, 20, 0);
+  VALUES('scorecard_section_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('scorecard_question_id_seq', 1, 20, 0);
+  VALUES('scorecard_question_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('project_id_seq', 1, 20, 0);
+  VALUES('project_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('project_phase_id_seq', 1, 20, 0);
+  VALUES('project_phase_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('resource_id_seq', 1, 20, 0);
+  VALUES('resource_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('resource_role_id_seq', 1, 20, 0);
+  VALUES('resource_role_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('notification_type_id_seq', 1, 20, 0);
+  VALUES('notification_type_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('upload_id_seq', 1, 20, 0);
+  VALUES('upload_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('upload_type_id_seq', 1, 20, 0);
+  VALUES('upload_type_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('upload_status_id_seq', 1, 20, 0);
+  VALUES('upload_status_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('submission_id_seq', 1, 20, 0);
+  VALUES('submission_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('submission_status_id_seq', 1, 20, 0);
+  VALUES('submission_status_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('review_id_seq', 1, 20, 0);
+  VALUES('review_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('review_item_id_seq', 1, 20, 0);
+  VALUES('review_item_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('review_comment_id_seq', 1, 20, 0);
+  VALUES('review_comment_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('review_item_comment_id_seq', 1, 20, 0);
+  VALUES('review_item_comment_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('project_audit_id_seq', 1, 20, 0);
+  VALUES('project_audit_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('screening_task_id_seq', 1, 20, 0);
+  VALUES('screening_task_id_seq', 5000, 10, 0);
 INSERT INTO id_sequences(name, next_block_start, block_size, exhausted)
-  VALUES('screening_result_id_seq', 1, 20, 0);
+  VALUES('screening_result_id_seq', 5000, 10, 0);
