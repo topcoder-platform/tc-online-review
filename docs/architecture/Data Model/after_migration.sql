1954068115a1db8847ab97db4a9268490cd11895
dulery <dulery@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-11-01 20:29:16 +0000

    Fixed typo

--- docs/architecture/Data Model/after_migration.sql
+++ docs/architecture/Data Model/after_migration.sql
@@ -2,7 +2,7 @@ update  project_info set value = '0' where value = '0.0';
 update  resource_info set value = '0' where value = '0.0';
 
 update project_info set value = replace(value, '.0', '') where value
-like '%.0'
+like '%.0';
 update resource_info set value = replace(value, '.0', '') where
 resource_info_type_id = 7 and value like '%.0'
 
