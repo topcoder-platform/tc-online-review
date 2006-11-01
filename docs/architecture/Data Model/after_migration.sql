1bad2930654b3a29026a0aba92b2cdac28783b10
dulery <dulery@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-11-01 20:24:49 +0000

    added after migration scripts

---
+++ docs/architecture/Data Model/after_migration.sql
@@ -0,0 +1,9 @@
+update  project_info set value = '0' where value = '0.0';
+update  resource_info set value = '0' where value = '0.0';
+
+update project_info set value = replace(value, '.0', '') where value
+like '%.0'
+update resource_info set value = replace(value, '.0', '') where
+resource_info_type_id = 7 and value like '%.0'
+
+update project_info set value=9926572 where value=0 and project_info_type_id=5;
