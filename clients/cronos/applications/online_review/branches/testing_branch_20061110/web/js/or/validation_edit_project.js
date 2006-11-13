bc800f143a3e03ddcb360f80f922b6d1a9ed826a
dulery <dulery@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-11-13 16:58:27 +0000

    Merged 4311

--- clients/cronos/applications/online_review/branches/testing_branch_20061110/web/js/or/validation_edit_project.js
+++ clients/cronos/applications/online_review/branches/testing_branch_20061110/web/js/or/validation_edit_project.js
@@ -233,9 +233,23 @@ function validate_timeline(thisForm, msgList) {
         msgDiv.style.display = "none";
         msgDiv.innerHTML = "";
         
+        var use_duration_true = thisForm["phase_use_duration[" + i + "]"][1].checked;
+        var use_duration_false = thisForm["phase_use_duration[" + i + "]"][0].checked;
+        
+        if (! (use_duration_true || use_duration_false)) {
+        	use_duration_true = true;
+        }
+        
         var end_date = thisForm["phase_end_date[" + i + "]"].value;
         var end_time = thisForm["phase_end_time[" + i + "]"].value;
         var empty_end_datetime = emptyString.test(end_date) && emptyString.test(end_time);
+        
+        if (use_duration_false) {
+        	if (empty_end_datetime) {
+        		msg = "End Date should be specified.";
+        		add_error_message(msg, msgPrefix, msgDiv, msgList);
+        	}
+        }
         
         if (!empty_end_datetime) {
             // if end date/time are specified, validate them.
@@ -252,19 +266,25 @@ function validate_timeline(thisForm, msgList) {
         var duration = thisForm["phase_duration[" + i + "]"].value;
         var empty_duration = emptyString.test(duration);
         
+        msgDiv = getChildByName(phase_row, "duration_validation_msg");
+        msgDiv.style.display = "none";
+        msgDiv.innerHTML = "";
+        
+        if (use_duration_true) {
+        	if (empty_duration) {
+        		msg = "Duration should be specified.";
+        		add_error_message(msg, msgPrefix, msgDiv, msgList);
+        	}
+        }
+        
         if (!empty_duration) {
             // if duration is specified, validate them.
-            if (!isTimeSpanString(duration)) {
+            if (!isTimeSpanString(duration)) {            
                 msg = "Duration should be in the form of \"hh\" or \"hh:mm\".";
                 add_error_message(msg, msgPrefix, msgDiv, msgList);
             }
         }
         
-        // either end datetime or duration must be specified.
-        if (empty_end_datetime && empty_duration) {
-            msg = "End DateTime or Duration Required.";
-            add_error_message(msg, msgPrefix, msgDiv, msgList);
-        }
     }
     
     return msg == null;
