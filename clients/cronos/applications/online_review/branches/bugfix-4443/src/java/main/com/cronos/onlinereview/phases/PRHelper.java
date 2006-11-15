f50e07ded8f87c3ee9e7875c57ed9427bdbe92c0
brain_cn <brain_cn@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-11-15 04:14:44 +0000

    OR-4443 : Project result record

--- clients/cronos/applications/online_review/branches/bugfix-4443/src/java/main/com/cronos/onlinereview/phases/PRHelper.java
+++ clients/cronos/applications/online_review/branches/bugfix-4443/src/java/main/com/cronos/onlinereview/phases/PRHelper.java
@@ -9,7 +9,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -96,10 +95,12 @@ class PRHelper {
      * @throws PhaseHandlingException if error occurs
      */
     static void processRegistrationPR(long projectId, Connection conn) throws SQLException {
+    	/* removed according to  	OR-4443
     	List submitters = getSubmitters(conn, projectId);
     	for (Iterator iter = submitters.iterator(); iter.hasNext();) {
     		insertProjectResult(conn, iter.next().toString(), projectId);
     	}
+    	*/
     }
 
     /**
@@ -112,10 +113,12 @@ class PRHelper {
     	PreparedStatement pstmt = null;
     	try {
     		// Ensure project_result exist
+    		/* removed according to  	OR-4443
         	List submitters = getSubmitters(conn, projectId);
         	for (Iterator iter = submitters.iterator(); iter.hasNext();) {
         		insertProjectResult(conn, iter.next().toString(), projectId);
         	}
+        	*/
 
         	// Update all users who submit submission
         	pstmt = conn.prepareStatement(UPDATE_PROJECT_RESULT_STMT);
@@ -276,87 +279,4 @@ class PRHelper {
     	close(pstmt);
     	return submitters;
     }
-
-    /**
-     * Insert project result for given user id and projectId, do nothing if project result exist.
-     * 
-     * @param conn the connection
-     * @param userId the user id
-     * @param projectId the project id
-     * @throws SQLException if error occurs while executing sql statement
-     */
-    static void insertProjectResult(Connection conn, String userId, long projectId) throws SQLException {
-        PreparedStatement ps = null;
-        ps = conn.prepareStatement("SELECT * FROM PROJECT_RESULT WHERE user_id = ? and project_id = ?");
-        ps.setString(1, userId);
-        ps.setLong(2, projectId);
-        ResultSet rs = ps.executeQuery();
-        if (rs.next()) {
-        	// the project result is exsting
-        	close(rs);
-        	close(ps);
-        	return;
-        }
-
-        close(rs);
-        close(ps);
-        
-        // prepare rating/Reliability
-        ps = conn.prepareStatement("SELECT rating from user_rating where user_id = ? and phase_id = " +
-                "(select 111+project_category_id from project where project_id = ?)");
-        ps.setString(1, userId);
-        ps.setLong(2, projectId);
-        rs = ps.executeQuery();
-
-        double old_rating = 0;
-
-        if (rs.next()) {
-            old_rating = rs.getLong(1);
-        }
-
-        close(rs);
-        close(ps);
-
-        ps = conn.prepareStatement("SELECT rating from user_reliability where user_id = ? and phase_id = " +
-                "(select 111+project_category_id from project where project_id = ?)");
-        ps.setString(1, userId);
-        ps.setLong(2, projectId);
-        rs = ps.executeQuery();
-
-        double oldReliability = 0;
-
-        if (rs.next()) {
-            oldReliability = rs.getDouble(1);
-        }
-
-        close(rs);
-        close(ps);
-
-        // add reliability_ind and old_reliability
-        ps = conn.prepareStatement("INSERT INTO project_result " +
-                "(project_id, user_id, rating_ind, reliability_ind, valid_submission_ind, old_rating, old_reliability) " +
-                "values (?, ?, ?, ?, ?, ?, ?)");
-
-        ps.setLong(1, projectId);
-        ps.setString(2, userId);
-        ps.setLong(3, 0);
-        ps.setLong(4, 0);
-        ps.setLong(5, 0);
-
-        if (old_rating == 0) {
-            ps.setNull(6, Types.DOUBLE);
-        } else {
-            ps.setDouble(6, old_rating);
-        }
-
-        if (oldReliability == 0) {
-            ps.setNull(7, Types.DOUBLE);
-        } else {
-            ps.setDouble(7, oldReliability);
-        }
-
-        ps.execute();
-        close(ps);
-    }
-
 }
