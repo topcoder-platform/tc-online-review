0b96e822d2bea5435d1b887b0cc39b3e924d17e5
pnussembaum <pnussembaum@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-02-20 18:54:00 +0000

    javadocs

--- clients/cronos/applications/online_review/branches/standalone/src/java/main/com/cronos/onlinereview/web/service/UsersService.java
+++ clients/cronos/applications/online_review/branches/standalone/src/java/main/com/cronos/onlinereview/web/service/UsersService.java
@@ -2,6 +2,21 @@ package com.cronos.onlinereview.web.service;
 
 import java.rmi.RemoteException;
 
+/**
+ * Defines the interface that must be provided by the UsersService implementors  
+ * 
+ * @author Bauna
+ */
 public interface UsersService {
+	
+	/**
+	 * Add a user as submitter to a project. 
+	 * If the user already has a role in the project 
+	 * this method fail throwing an exception
+	 * 
+	 * @param projectId the projects's id
+	 * @param userId the user's id
+	 * @throws RemoteException if any error occurs.
+	 */
 	void addSubmitter(long projectId, long userId) throws RemoteException;
 }
