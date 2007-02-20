96feb2af1440c395e58c1cbaff8cf21e2a0dcdde
brain_cn <brain_cn@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-02-20 08:26:54 +0000

    Copied remotely

---
+++ clients/cronos/applications/online_review/branches/bugfix-4899/ApplicationTests/src/java/tests/com/cronos/onlinereview/tests/projects/submissions/ViewSubmissionTest.java
@@ -0,0 +1,32 @@
+/* Copyright (C) 2007 TopCoder Inc., All Rights Reserved. */
+
+package com.cronos.onlinereview.tests.projects.submissions;
+
+import com.cronos.onlinereview.AbstractTestCase;
+
+public class ViewSubmissionTest extends AbstractTestCase
+{
+    public void testScenario082()
+    {
+    }
+
+    public void testScenario083()
+    {
+    }
+
+    public void testScenario084()
+    {
+    }
+
+    public void testScenario085()
+    {
+    }
+
+    public void testScenario086()
+    {
+    }
+
+    public void testScenario087()
+    {
+    }
+}
