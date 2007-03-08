a70214f5b33f9f7bb3867a4965924369b766822d
pnussembaum <pnussembaum@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-03-08 18:54:41 +0000

    Classes to obtain the PactServices Session Bean

---
+++ clients/cronos/applications/online_review/branches/IP_assignment/src/java/main/com/cronos/onlinereview/phases/ServiceLocatorCreateException.java
@@ -0,0 +1,23 @@
+package com.cronos.onlinereview.phases;
+
+import com.topcoder.util.errorhandling.BaseException;
+
+public class ServiceLocatorCreateException extends BaseException {
+
+	public ServiceLocatorCreateException() {
+		super();
+	}
+
+	public ServiceLocatorCreateException(String message) {
+		super(message);
+	}
+
+	public ServiceLocatorCreateException(Throwable cause) {
+		super(cause);
+	}
+
+	public ServiceLocatorCreateException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
