be98cc97e66de9aaa53244457ecafc1efe2d2beb
pnussembaum <pnussembaum@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-05-17 16:33:38 +0000

    bugfix-4424

--- clients/cronos/applications/online_review/trunk/scripts/auto_screening/checkstyle.sh
+++ clients/cronos/applications/online_review/trunk/scripts/auto_screening/checkstyle.sh
@@ -1,2 +1,4 @@
-/usr/java/jdk1.5.0_06/bin/java  -classpath /home/onlinereview/online_review/lib/third_party/checkstyle-all-4.2.jar com.puppycrawl.tools.checkstyle.Main -c tc_checks.xml -r $1
+script_path=$(dirname $0)
+
+/usr/local/java/jdk1.5.0_08/bin/java -classpath $script_path/lib/checkstyle-all-4.2.jar com.puppycrawl.tools.checkstyle.Main -c tc_checks.xml -r $1
 exit 0
