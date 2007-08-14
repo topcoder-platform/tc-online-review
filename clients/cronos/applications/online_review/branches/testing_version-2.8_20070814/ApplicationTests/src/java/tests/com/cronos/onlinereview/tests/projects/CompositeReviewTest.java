2f6b266ef34d1871fb8e7e2ed0995dcabe98b4e0
pnussembaum <pnussembaum@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-08-14 17:00:08 +0000

    branch for version 2.8

---
+++ clients/cronos/applications/online_review/branches/testing_version-2.8_20070814/ApplicationTests/src/java/tests/com/cronos/onlinereview/tests/projects/CompositeReviewTest.java
@@ -0,0 +1,16 @@
+/* Copyright (C) 2007 TopCoder Inc., All Rights Reserved. */
+
+package com.cronos.onlinereview.tests.projects;
+
+import com.cronos.onlinereview.AbstractTestCase;
+
+public class CompositeReviewTest extends AbstractTestCase
+{
+    public void testScenario205()
+    {
+    }
+
+    public void testScenario206()
+    {
+    }
+}
