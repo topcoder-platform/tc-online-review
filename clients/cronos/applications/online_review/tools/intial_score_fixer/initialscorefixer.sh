e8c4ec715dd631d0f85c8adea77e348467d5b142
pnussembaum <pnussembaum@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-08-16 16:28:29 +0000

    dinamically load jars

--- clients/cronos/applications/online_review/tools/intial_score_fixer/initialscorefixer.sh
+++ clients/cronos/applications/online_review/tools/intial_score_fixer/initialscorefixer.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-CP=dist/base_exception.jar:dist/class_associations.jar:dist/command_line_utility.jar:\
-	dist/configuration_manager.jar:dist/data_validation.jar:dist/database_abstraction.jar:\
-	dist/db_connection_factory.jar:dist/id_generator.jar:dist/logging_wrapper.jar:\
-	dist/object_factory.jar:dist/review_data_structure.jar:dist/review_management.jar:\
-	dist/review_management_persistence.jar:dist/review_score_calculator.jar:\
-	dist/scorecard_data_structure.jar:dist/scorecard_management.jar:\
-	dist/scorecard_management_persistence.jar:dist/search_builder.jar:\
-	dist/simple_cache.jar:dist/typesafe_enum.jar:dist/weighted_calculator.jar:\
-	dist/ifx-jdbc.jar:build/classes:conf
+CP="build/classes:conf"
+for i in $(find dist -name \*jar); do
+	CP="$CP:$i"
+done
 
 MAIN_CLASS=com.topcoder.management.review.scorefixer.InitialScoreFixer
 
