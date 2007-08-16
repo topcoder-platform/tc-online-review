5b6e43bd39f4aa2fdd24022431f0e0ca197e14a9
pnussembaum <pnussembaum@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-08-16 13:41:36 +0000

    moved to initial_score_fixer

--- clients/cronos/applications/online_review/tools/initialscorefixer.sh
+++
@@ -1,3 +0,0 @@
-export CLASSPATH=dist/base_exception.jar:dist/class_associations.jar:dist/command_line_utility.jar:dist/configuration_manager.jar:dist/data_validation.jar:dist/database_abstraction.jar:dist/db_connection_factory.jar:dist/id_generator.jar:dist/logging_wrapper.jar:dist/object_factory.jar:dist/review_data_structure.jar:dist/review_management.jar:dist/review_management_persistence.jar:dist/review_score_calculator.jar:dist/scorecard_data_structure.jar:dist/scorecard_management.jar:dist/scorecard_management_persistence.jar:dist/search_builder.jar:dist/simple_cache.jar:dist/typesafe_enum.jar:dist/weighted_calculator.jar:dist/ifx-jdbc.jar:build/classes:conf
-
-java com.topcoder.management.review.scorefixer.InitialScoreFixer $*
