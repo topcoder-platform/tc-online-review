0403fb26356d0fd19f224e3b920d797fcd6ea837
George1 <George1@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-09-12 09:55:40 +0000

    branched.

---
+++ clients/cronos/applications/online_review/branches/bugfix-7536/web/js/or/popup.js
@@ -0,0 +1,8 @@
+var win = null;
+function NewWindow(mypage,myname,w,h,scroll){
+LeftPosition = (screen.width) ? (screen.width-w)/2 : 0;
+TopPosition = (screen.height) ? (screen.height-h)/2 : 0;
+settings =
+'height='+h+',width='+w+',top='+TopPosition+',left='+LeftPosition+',scrollbars='+scroll+',resizable'
+win = window.open(mypage,myname,settings)
+}
