c3c5c2fc50e972e39094e5b9df8827ef94057496
George1 <George1@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-09-14 18:24:36 +0000

    branched.

---
+++ clients/cronos/applications/online_review/branches/bugfix-10156/ApplicationTests/src/java/tests/com/cronos/onlinereview/tests/projects/aggregation/PerformAggregationTest.java
@@ -0,0 +1,32 @@
+/* Copyright (C) 2007 TopCoder Inc., All Rights Reserved. */
+
+package com.cronos.onlinereview.tests.projects.aggregation;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.cronos.onlinereview.AbstractTestCase;
+import com.cronos.onlinereview.Configuration;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public class PerformAggregationTest extends AbstractTestCase
+{
+    public void testScenario126()
+    {
+    }
+
+    public void testScenario127()
+    {
+    }
+
+    public void testScenario128()
+    {
+    }
+
+    public void testScenario129()
+    {
+    }
+
+    public void testScenario130()
+    {
+    }
+}
