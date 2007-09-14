5fc70baba6e60e46bc3f4364876975614cb15d59
George1 <George1@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-09-14 18:07:13 +0000

    bracnhed.

---
+++ clients/cronos/applications/online_review/branches/bugfix-10153/ApplicationTests/src/java/tests/com/cronos/onlinereview/tests/projects/endphase/EndPhaseApprovalTest.java
@@ -0,0 +1,24 @@
+/* Copyright (C) 2007 TopCoder Inc., All Rights Reserved. */
+
+package com.cronos.onlinereview.tests.projects.endphase;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.cronos.onlinereview.AbstractTestCase;
+import com.cronos.onlinereview.Configuration;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public class EndPhaseApprovalTest extends AbstractTestCase
+{
+    public void testScenario193()
+    {
+    }
+
+    public void testScenario194()
+    {
+    }
+
+    public void testScenario195()
+    {
+    }
+}
