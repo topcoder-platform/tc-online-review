d6ded622ca95c753ed0aea4f083a353fb2f16b93
gpaul <gpaul@fb370eea-3af6-4597-97f7-f7400a59c12a>
2008-06-10 19:37:06 +0000

    [OR-584] take out the logic for the v1 assignment document

--- clients/cronos/applications/online_review/trunk/src/java/main/com/cronos/onlinereview/phases/PRAppealResponsePhaseHandler.java
+++ clients/cronos/applications/online_review/trunk/src/java/main/com/cronos/onlinereview/phases/PRAppealResponsePhaseHandler.java
@@ -6,9 +6,6 @@ package com.cronos.onlinereview.phases;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.text.MessageFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import com.cronos.onlinereview.external.ExternalUser;
 import com.topcoder.management.deliverable.Submission;
@@ -16,7 +13,6 @@ import com.topcoder.management.deliverable.UploadManager;
 import com.topcoder.management.phase.PhaseHandlingException;
 import com.topcoder.management.phase.PhaseManagementException;
 import com.topcoder.management.project.Project;
-import com.topcoder.management.project.ProjectCategory;
 import com.topcoder.management.resource.Resource;
 import com.topcoder.management.resource.ResourceRole;
 import com.topcoder.management.resource.search.ResourceFilterBuilder;
@@ -32,7 +28,6 @@ import com.topcoder.util.file.fieldconfig.Field;
 import com.topcoder.util.file.fieldconfig.Node;
 import com.topcoder.util.file.fieldconfig.TemplateFields;
 import com.topcoder.util.log.Level;
-import com.topcoder.web.ejb.pacts.assignmentdocuments.AssignmentDocument;
 
 /**
  * The extend from AppealsResponsePhaseHandler to add on the logic to push data to project_result.
@@ -59,18 +54,24 @@ public class PRAppealResponsePhaseHandler extends AppealsResponsePhaseHandler {
 	private String winnersEmailFromAddress;
 	
 	/** link to the accept the Assignment Document */
+/*[OR-584] we're not doing assignment like this anymore, v2 only now
 	private String winnersEmailAssignmentDocumentLink;
-	
+*/
+
 	/** helper class for obtaining several managers */
 	private ManagerHelper managerHelper;
 
-	/** switch that indicates if OR have to create Assignment Documents in PACTs*/
-	private boolean createAssignmentDocuments = false;
-	
-	/** start date to check or create assigments documents*/
+    /** start date to check or create assigments documents*/
+/*[OR-584] we're not doing assignment like this anymore, v2 only now
 	private Date assignmentDocumentsStartDate;
-	
-	/**
+*/
+
+    /** switch that indicates if OR have to create Assignment Documents in PACTs*/
+/*[OR-584] we're not doing assignment like this anymore, v2 only now
+    private boolean createAssignmentDocuments = false;
+*/
+
+    /**
      * Create a new instance of AppealsResponsePhaseHandler using the default namespace for loading configuration settings.
      *
      * @throws ConfigurationException if errors occurred while loading configuration settings.
@@ -94,8 +95,11 @@ public class PRAppealResponsePhaseHandler extends AppealsResponsePhaseHandler {
 	}
 	
     private void obtainWinnnersEmailConfigProperties(String namespace) throws ConfigurationException {
+/* [OR-584] we're not doing assignment like this anymore, v2 only now
     	this.createAssignmentDocuments = "true".equals(PhasesHelper.getPropertyValue(namespace, "CreateAssignmentDocuments", "false").trim());
     	if (createAssignmentDocuments) {
+*/
+/*
     		try {
     			SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
     			sdf.setLenient(false);
@@ -103,14 +107,19 @@ public class PRAppealResponsePhaseHandler extends AppealsResponsePhaseHandler {
     		} catch (ParseException e) {
     			throw new ConfigurationException("can not read AssignmentDocumentsStartDate from configuration", e);
     		}
+*/
     		this.winnersEmailTemplateSource = PhasesHelper.getPropertyValue(namespace, "WinnersEmail.EmailTemplateSource", true);
     		this.winnersEmailTemplateName = PhasesHelper.getPropertyValue(namespace, "WinnersEmail.EmailTemplateName",  true);
     		this.winnersEmailSubject = PhasesHelper.getPropertyValue(namespace, "WinnersEmail.EmailSubject", true);
+/*
     		this.winnersEmailAssignmentDocumentLink = PhasesHelper.getPropertyValue(namespace, "WinnersEmail.AssignmentDocumentLink", true);
-    		this.winnersEmailFromAddress = PhasesHelper.getPropertyValue(namespace, "WinnersEmail.EmailFromAddress", true);	
+*/
+    		this.winnersEmailFromAddress = PhasesHelper.getPropertyValue(namespace, "WinnersEmail.EmailFromAddress", true);
+/*
     	} else {
     		log.log(Level.INFO, "Creation of Assignment Documents is disabled"); 
     	}
+*/
 	}
 
 	/**
@@ -132,9 +141,15 @@ public class PRAppealResponsePhaseHandler extends AppealsResponsePhaseHandler {
     	Connection conn = this.createConnection();
     	try {
     		processPR(projectId, conn, toStart);
-    		if (!toStart && isCreateAssignmentDocuments()) {
-    			createAssignmentDocuments(projectId);
-    		}
+            //send winner email as appropriate here
+            if (!toStart) {
+                sendMailForWinners(getManagerHelper().getProjectManager().getProject(projectId));
+            }
+/* [OR-584] don't need to do this anymore, we only accept v2 documents now
+            if (!toStart && isCreateAssignmentDocuments()) {
+                createAssignmentDocuments(projectId);
+            }
+*/
     	} catch (Throwable e) {
 			throw new PhaseHandlingException(e.getMessage(), e);
 		} finally {
@@ -148,19 +163,21 @@ public class PRAppealResponsePhaseHandler extends AppealsResponsePhaseHandler {
      * @param project the project
      * @return true if assignment documents must to be created.
      */
+/* [OR-584] don't nee this anymore, we're only doing v2 assignment now
     private boolean verifyCreateAssignmentDocument(Project project) {
     	ProjectCategory category = project.getProjectCategory();
     
         return project.getCreationTimestamp().after(assignmentDocumentsStartDate)
         		&& (category.getId() == 1 || category.getId() == 2);
     }
-    
+*/
+
     /**
      * Creates the Assigment Documents for the winners of the project if there are any.
      * 
-     * @param projectId the project id.
-     * @throws Exception 
+     * @throws Exception
      */
+/* [OR-584] don't need this anymore, we're only doing v2 assignment
     private void createAssignmentDocuments(long projectId) throws Exception {
     	Project project = getManagerHelper().getProjectManager().getProject(projectId);
     	if (verifyCreateAssignmentDocument(project)) {
@@ -170,25 +187,28 @@ public class PRAppealResponsePhaseHandler extends AppealsResponsePhaseHandler {
     		log.log(Level.INFO, "Don't creating AD for project: " + projectId + " because it's creation date is: " + project.getCreationTimestamp());
     	}
 	}
+*/
 
-	private void sendMailForWinners(Project project, AssignmentDocumentResult adResult) throws Exception {
-		if (adResult.hasWinner()) {
-			AssignmentDocument winnerAD = adResult.getWinnerAssignmentDocument();
-			sendWinnersEmailForUser(project, winnerAD, managerHelper.getUserRetrieval().retrieveUser(
-					winnerAD.getUser().getId().longValue()), "1st");
+	private void sendMailForWinners(Project project) throws Exception {
+        log.log(Level.DEBUG, "we're in the send email method");
+       String winnerId = (String) project.getProperty("Winner External Reference ID");
+       String runnerUpId = (String) project.getProperty("Runner-up External Reference ID");
+
+        if (winnerId!=null) {
+			sendWinnersEmailForUser(project,
+                    managerHelper.getUserRetrieval().retrieveUser(Long.parseLong(winnerId)), "1st");
 		}
-		if (adResult.hasRunnerUp()) {
-			AssignmentDocument runnerUpAD = adResult.getRunnerUpAssignmentDocument();
-			sendWinnersEmailForUser(project, runnerUpAD, managerHelper.getUserRetrieval().retrieveUser(
-					runnerUpAD.getUser().getId().longValue()), "2nd");
+		if (runnerUpId!=null) {
+			sendWinnersEmailForUser(project,
+                    managerHelper.getUserRetrieval().retrieveUser(Long.parseLong(runnerUpId)), "2nd");
 		}
 	}
 
-	private void sendWinnersEmailForUser(Project project, AssignmentDocument ad, ExternalUser user, String position) throws Exception {
+	private void sendWinnersEmailForUser(Project project, ExternalUser user, String position) throws Exception {
 		DocumentGenerator docGenerator = DocumentGenerator.getInstance();
 		Template template = getEmailTemplate();
 		log.log(Level.DEBUG, "sending winner email for projectId: " + project.getId() + " handle: " + user.getHandle() + " position: " + position);
-		TemplateFields root = setTemplateFieldValues(docGenerator.getFields(template), project, user, ad, position);
+		TemplateFields root = setTemplateFieldValues(docGenerator.getFields(template), project, user, position);
 		
 		String emailContent = docGenerator.applyTemplate(root);
 		TCSEmailMessage message = new TCSEmailMessage();
@@ -235,7 +255,7 @@ public class PRAppealResponsePhaseHandler extends AppealsResponsePhaseHandler {
 		throw new PhaseHandlingException("couldn't found the resource for userId: " + userId + " projectId: " + project.getId());
 	}
 	
-	private TemplateFields setTemplateFieldValues(TemplateFields root, Project project, ExternalUser user, AssignmentDocument ad, String position) throws BaseException {
+	private TemplateFields setTemplateFieldValues(TemplateFields root, Project project, ExternalUser user, String position) throws BaseException {
 		Node[] nodes = root.getNodes();
 
 		for (int i = 0; i < nodes.length; i++) {
@@ -262,9 +282,10 @@ public class PRAppealResponsePhaseHandler extends AppealsResponsePhaseHandler {
                     }
 				} else if ("PLACE".equals(field.getName())) {
 					field.setValue(position);
-				} else if ("ASSIGNMET_DOCUMENT_LINK".equals(field.getName())) {
+				} /* [OR-584] don't need this link anymore
+				else if ("ASSIGNMET_DOCUMENT_LINK".equals(field.getName())) {
 					field.setValue(MessageFormat.format(winnersEmailAssignmentDocumentLink, new Object[] {ad.getId().toString()}));
-				} 
+				} */
 			}
 		}
 
@@ -289,7 +310,9 @@ public class PRAppealResponsePhaseHandler extends AppealsResponsePhaseHandler {
     	}
     }
 
+/* [OR-584] we're not doing assignment like this anymore, v2 only now
 	public boolean isCreateAssignmentDocuments() {
 		return createAssignmentDocuments;
 	}
+*/
 }
