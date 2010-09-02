7bae8dd73d42fb7617c82d8c3d4097536aa24f1e
isv <isv@fb370eea-3af6-4597-97f7-f7400a59c12a>
2010-09-02 15:38:10 +0000

    Debugging perfomance loss issue

--- clients/cronos/applications/online_review/branches/spec_review_assemply_part1_isv/src/java/main/com/cronos/onlinereview/actions/PhasesDetailsServices.java
+++ clients/cronos/applications/online_review/branches/spec_review_assemply_part1_isv/src/java/main/com/cronos/onlinereview/actions/PhasesDetailsServices.java
@@ -602,6 +602,9 @@ final class PhasesDetailsServices {
                 Filter filterStatus = ActionsHelper.createSubmissionStatusFilter(allSubmissionStatuses);
                 Filter filterResource = SubmissionFilterBuilder.createResourceIdFilter(myResource.getId());
                 Filter filterType = SubmissionFilterBuilder.createSubmissionTypeIdFilter(submissionType.getId());
+                System.out.println("ISV : project.getId() = " + project.getId());
+                System.out.println("ISV : myResource.getId() = " + myResource.getId());
+                System.out.println("ISV : submissionType.getId() = " + submissionType.getId());
 
                 Filter filter = new AndFilter(Arrays.asList(filterProject, filterStatus, filterResource, filterType));
 
