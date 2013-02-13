fa00222a69d3c813731e3e81fae7d727630d8af4
isv <isvisv@users.noreply.github.com>
2013-02-13 14:45:27 +0000

    Restored all test suites for running on new CI server

--- src/java/tests/com/cronos/onlinereview/FunctionalTests.java
+++ src/java/tests/com/cronos/onlinereview/FunctionalTests.java
@@ -19,32 +19,32 @@ public class FunctionalTests extends TestCase {
 
     public static Test suite() {
         final TestSuite suite = new TestSuite();
-//        suite.addTestSuite(LoginTests.class);
+        suite.addTestSuite(LoginTests.class);
         suite.addTestSuite(ManageProjectTests.class);
-//        suite.addTestSuite(LinkProjectsTests.class);
-//        suite.addTestSuite(OpenPhaseTests.class);
-//        suite.addTestSuite(EditProjectTests.class);
-//        suite.addTestSuite(ClosePhaseTests.class);
-//        suite.addTestSuite(ManageProjectWithNoPermissionTests.class);
-//        suite.addTestSuite(SettingPhaseEndDateTests.class);
-//        suite.addTestSuite(AddRoleTests.class);
-//        suite.addTestSuite(FinalFixSubmissionUploadTests.class);
-//        suite.addTestSuite(SubmissionUploadTests.class);
-//        suite.addTestSuite(EditProjectFiledsTests.class);
-//        suite.addTestSuite(CompetitorPrivilegeTests.class);
-//        suite.addTestSuite(ScoreCardTests.class);
-//        suite.addTestSuite(DeleteReviewerTests.class);
-//        suite.addTestSuite(ChangeScorecardTests.class);
+        suite.addTestSuite(LinkProjectsTests.class);
+        suite.addTestSuite(OpenPhaseTests.class);
+        suite.addTestSuite(EditProjectTests.class);
+        suite.addTestSuite(ClosePhaseTests.class);
+        suite.addTestSuite(ManageProjectWithNoPermissionTests.class);
+        suite.addTestSuite(SettingPhaseEndDateTests.class);
+        suite.addTestSuite(AddRoleTests.class);
+        suite.addTestSuite(FinalFixSubmissionUploadTests.class);
+        suite.addTestSuite(SubmissionUploadTests.class);
+        suite.addTestSuite(EditProjectFiledsTests.class);
+        suite.addTestSuite(CompetitorPrivilegeTests.class);
+        suite.addTestSuite(ScoreCardTests.class);
+        suite.addTestSuite(DeleteReviewerTests.class);
+        suite.addTestSuite(ChangeScorecardTests.class);
 
-//        suite.addTestSuite(ReviewerManageScorecardTests.class);
-//        suite.addTestSuite(ReviewerUploadTestCaseTests.class);
-//        suite.addTestSuite(ManageAppealTests.class);
-//        suite.addTestSuite(ScreenPhaseCannotOpenTests.class);
-//        suite.addTestSuite(UnregisterProjectTests.class);
-//        suite.addTestSuite(PostmortemReviewerManageScorecardTests.class);
-//        suite.addTestSuite(ScreenerManageScorecardTests.class);
-//        suite.addTestSuite(AggregatorAddScorecardTests.class);
-//        suite.addTestSuite(TimelineAdvancementTests.class);
+        suite.addTestSuite(ReviewerManageScorecardTests.class);
+        suite.addTestSuite(ReviewerUploadTestCaseTests.class);
+        suite.addTestSuite(ManageAppealTests.class);
+        suite.addTestSuite(ScreenPhaseCannotOpenTests.class);
+        suite.addTestSuite(UnregisterProjectTests.class);
+        suite.addTestSuite(PostmortemReviewerManageScorecardTests.class);
+        suite.addTestSuite(ScreenerManageScorecardTests.class);
+        suite.addTestSuite(AggregatorAddScorecardTests.class);
+        suite.addTestSuite(TimelineAdvancementTests.class);
         
         return suite;
     }
