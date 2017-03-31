c5c36f590820adde77a744ba1ce346b3c50a3460
Guiqiang Zhang <skyhit@gmail.com>
2017-03-31 11:36:25 +0800

    build from circleci

---
+++ .deploy/circleci/build.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+set -e
+
+mv .deploy/circleci/build.properties .
+mv .deploy/circleci/token.properties .
+#add svn user name and password to build.properies
+echo "build.svn.username=$SVN_USERNAME" >> build.properties
+echo "build.svn.password=$SVN_PASSWORD" >> build.properties
+ant checkout-components 
+ant first_deploy deploy
+
