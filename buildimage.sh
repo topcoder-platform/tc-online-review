28e4b1ffe03da581cca7115227c45c2d720217b3
Gunasekar K <gunasekark@L-156161609.local>
2018-10-08 17:25:31 +0530

    app deployment changes

--- buildimage.sh
+++ buildimage.sh
@@ -5,7 +5,7 @@ AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
 AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
 AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
-AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY")
+#AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY")
 
 TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_BUILD_NUM
 docker build --rm -f "docker/Dockerfile.base" -t or-dev-base:latest docker
