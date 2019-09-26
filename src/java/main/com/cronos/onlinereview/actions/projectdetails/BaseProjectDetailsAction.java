9c5b2343a18a63d762ea51ad0774f3253c0cda03
Dushyant Bhalgami <dushyantbhalgami@gmail.com>
2019-09-26 12:32:57 +0530

    fixed sumission download restrict for MM

--- src/java/main/com/cronos/onlinereview/actions/projectdetails/BaseProjectDetailsAction.java
+++ src/java/main/com/cronos/onlinereview/actions/projectdetails/BaseProjectDetailsAction.java
@@ -461,6 +461,18 @@ public abstract class BaseProjectDetailsAction extends DynamicModelDrivenAction 
                         this, request, "ViewSubmission", "Error.NoScreeningPassed", null);
             }
 
+            // Allow only submitter to download the submission if it is MM type of challenge
+            if (project.getProjectCategory().getId() == 37) {
+                if (!Boolean.parseBoolean((String) project.getProperty("Viewable Submissions Flag"))) {
+                    long submitter =  upload.getOwner();
+                    long loggedInUserId = AuthorizationHelper.getLoggedInUserId(request);
+                    if (submitter != loggedInUserId) {
+                        return ActionsHelper.produceErrorReport(this, request, errorMessageKey, "Error.NoPermission",
+                            Boolean.FALSE);
+                    }
+                }  
+            }
+
             // Submitters can download others' contest submissions and checkpoint submissions only
             // after the Appeals Response phase (or Review phase if Appeals Response phase is not present)
             // is closed and only if at least one submission passed review.
