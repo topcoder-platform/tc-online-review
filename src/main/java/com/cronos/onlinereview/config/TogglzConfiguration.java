37b7be1ae26b70a345ac007e9e3e89a24d6e61f3
xxcxy <bert7914@gmail.com>
2022-06-02 11:08:42 +0800

    fix timeline error

--- src/main/java/com/cronos/onlinereview/config/TogglzConfiguration.java
+++ src/main/java/com/cronos/onlinereview/config/TogglzConfiguration.java
@@ -1,15 +1,29 @@
 package com.cronos.onlinereview.config;
 
+import com.cronos.onlinereview.util.AuthorizationHelper;
+import com.topcoder.onlinereview.component.security.RolePrincipal;
+import com.topcoder.onlinereview.component.security.login.LoginBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.togglz.core.Feature;
 import org.togglz.core.manager.TogglzConfig;
 import org.togglz.core.repository.StateRepository;
 import org.togglz.core.repository.file.FileBasedStateRepository;
 import org.togglz.core.user.SimpleFeatureUser;
 import org.togglz.core.user.UserProvider;
+import org.togglz.servlet.util.HttpServletRequestHolder;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.util.List;
+import java.util.Set;
 
 public class TogglzConfiguration implements TogglzConfig {
+    private static final Logger logger = LoggerFactory.getLogger(TogglzConfiguration.class);
+
+    @Value("#{'${togglz.roles}'.split(',')}")
+    private List<String> roles;
 
     public Class<? extends Feature> getFeatureClass() {
         return TogglzFeatures.class;
@@ -20,6 +34,15 @@ public class TogglzConfiguration implements TogglzConfig {
     }
 
     public UserProvider getUserProvider() {
-        return () -> new SimpleFeatureUser("admin", true);
+        return () -> {
+            HttpServletRequest request = HttpServletRequestHolder.get();
+            Set<RolePrincipal> roleSet = LoginBean.getUserRoles(AuthorizationHelper.getLoggedInUserId(request));
+            for (RolePrincipal role : roleSet) {
+                if (roles.contains(role.getName())) {
+                    return new SimpleFeatureUser("admin", true);
+                }
+            }
+            return new SimpleFeatureUser("user", false);
+        };
     }
 }
