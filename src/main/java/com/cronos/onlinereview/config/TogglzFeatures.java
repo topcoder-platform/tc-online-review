42db1f09b73f4bb8bdc11b1997b09356df91aff3
Emre <emre.isbilir@gmail.com>
2022-08-02 08:59:20 +0300

    enable kafka switch by default

--- src/main/java/com/cronos/onlinereview/config/TogglzFeatures.java
+++ src/main/java/com/cronos/onlinereview/config/TogglzFeatures.java
@@ -1,11 +1,13 @@
 package com.cronos.onlinereview.config;
 
 import org.togglz.core.Feature;
+import org.togglz.core.annotation.EnabledByDefault;
 import org.togglz.core.annotation.Label;
 import org.togglz.core.context.FeatureContext;
 
 public enum TogglzFeatures implements Feature {
 
+    @EnabledByDefault
     @Label("Send kafka message")
     SEND_KAFKA_MESSAGE;
 
